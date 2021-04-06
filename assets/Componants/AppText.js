ae543aacc0ae66f5ec530d132cb952965ab6a0f6
JenniferJohnstone <jennifermaryjohnstone@gmail.com>
2021-04-06 14:00:11 +1000

    login

--- assets/Componants/AppText.js
+++ assets/Componants/AppText.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text , StyleSheet} from 'react-native';
 
+
 function AppText({children, style}) {
 
-
-    return (
+      return (
         <Text style={styles.text, style}>
             {children}
         </Text>
--- assets/Screens/Login.js
+++ assets/Screens/Login.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { Image, StyleSheet, TextInput, View } from 'react-native';
+import React, {useState} from 'react';
+import { Image, StyleSheet, TextInput, View, Text } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import {useFonts, VarelaRound_400Regular} from '@expo-google-fonts/varela-round'
+
 
 import AppView from '../Componants/AppView'
 import AppColors from '../Config/appColors'
+import AppText from '../Componants/AppText'
 
 function Login() {
+
+   const [email, setEmail] = useState(null)
+   const [password, setPassword] = useState(null)
+
+    // I haven't figured out a better way to use a custom font, if I try to do this same trick with AppText it doesn't work 
+
+    let [fonts_loaded] = useFonts({
+        VarelaRound_400Regular,
+    })
+
+    if(!fonts_loaded){
+        return <Text> App Loading </Text>
+    }
+
+
     return (
         <AppView style={styles.screen}>
 
             <Image source={require('../images/globe.png')} style={styles.image} />
+
+
+
             <View style={styles.underline}>
-                <MaterialCommunityIcons name="email" size={20} color={AppColors.Blue} style={{ padding: 0 }} />
-                <TextInput placeholder="Email" style={styles.input}></TextInput>
+                <MaterialCommunityIcons name="email" size={20} color={AppColors.Blue} style={{ paddingTop: 30 }}/>
+                <TextInput placeholder="Email" style={styles.input} value={email} onChangeText={setEmail} keyboardType="email-address" textContentType="emailAddress" ></TextInput>
             </View>
+            <View style={styles.underline}>
+                <MaterialCommunityIcons name="lock" size={20} color={AppColors.Blue} style={{ paddingTop: 30 }} />
+                <TextInput placeholder="Password" style={styles.input} value={password} onChangeText={setPassword} textContentType="password"></TextInput>
+            </View>
+
+
+
+            <AppText style={styles.password}>Forgot Password?</AppText>
+
+            <AppText style={styles.registration}>Don't have an account yet?</AppText>
+            <AppText style={[styles.password, {fontSize: 15, paddingTop: 5}]}>Sign Up</AppText>
 
 
 
@@ -28,18 +60,33 @@ const styles = StyleSheet.create({
     },
     screen: {
         alignItems: 'center',
-        backgroundColor: AppColors.Green
+        backgroundColor: AppColors.Green,
     },
     input: {
         width: 200,
         textAlign: 'center',
         color: AppColors.Yellow,
         fontSize: 20,
+        fontFamily: 'VarelaRound_400Regular',
+        padding: 10, 
+        paddingTop: 30 
+
     },
     underline: {
         borderBottomWidth: 2,
         borderBottomColor: AppColors.Orange,
         flexDirection: 'row',
+    },
+    password: {
+        color: AppColors.DarkRed, 
+        fontSize: 18,
+        fontFamily: 'VarelaRound_400Regular',
+        paddingTop: 25
+    }, 
+    registration: {
+        color: AppColors.Yellow, 
+        fontSize: 15,
+        paddingTop: 100,
     }
 })
 
